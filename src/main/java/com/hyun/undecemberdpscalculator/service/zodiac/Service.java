7c7192adea5966fe6d91ac5995b9d5b35864dcfb
dbs43 <dyun4374@gmail.com>
2022-05-04 05:48:56 +0900

    com

---
+++ src/main/java/com/hyun/undecemberdpscalculator/service/zodiac/Service.java
@@ -0,0 +1,15 @@
+package com.hyun.undecemberdpscalculator.service.zodiac;
+
+
+import com.hyun.undecemberdpscalculator.dto.DmgDto;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+public class Service {
+    DmgDto dto = new DmgDto();
+
+//    public int test() {
+//
+//    }
+}
